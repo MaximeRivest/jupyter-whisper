a4adcc562ad191db867892dcd4cca3266ac8e175
Maxime Rivest <mrive052@gmail.com>
2024-11-03 10:58:28 -0500

    docs: update Sphinx configuration

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+project = 'JupyterChat'
+copyright = '2024, Maxime Rivest'
+author = 'Maxime Rivest'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx_rtd_theme',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosectionlabel',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'jupyter': ('https://jupyter.org/documentation', None),
+}
+
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
